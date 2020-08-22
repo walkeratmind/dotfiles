1f5954fbbc08693bbe01904a2363cba83776334f
Rakesh Niraula <rakeshhniraulaa@gmail.com>
2020-08-22 19:04:33 +0545

    add ydl aliases

--- .shellrc.d/utils.sh
+++ .shellrc.d/utils.sh
@@ -155,35 +155,6 @@ function dlna {
 # }
 
 
-function ydl {
-    local list=$(youtube-dl --list-formats $1)
-
-    echo $list | sed -n '/[0-9]x[0-9]/p'
-    echo -n "video format (default=244, skip=0): "; read video
-    if [[ "$video" == 0 ]]; then
-        video=""
-    else
-        video="${video:-244}"
-    fi
-
-    echo $list | sed -n '/audio only/p'
-    total=$(echo $list | sed -n '/audio only/p' | wc -l)
-    # TODO: check 250 is in or not
-    echo -n "audio format (default=250, skip=0): "; read audio
-    if [[ "$audio" == 0 ]]; then
-        audio=""
-    else
-        audio="${audio:-250}"
-        if [[ "$video" != "" ]]; then
-            audio="+$audio"
-        fi
-    fi
-
-    echo youtube-dl --format "${video}${audio}" $1
-    youtube-dl --format "${video}${audio}" $1
-}
-
-
 function randpass {
     #random password of desired length
     < /dev/urandom tr -dc '_A-Z-a-z-0-9+-~!@#$%^&*()_+=-'| head -c${1:-16};echo;
