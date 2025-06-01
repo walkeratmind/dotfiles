fc2a957e8fb18af61bd2c8e564c2a6f533a09a2f
Rakesh <rakeshhniraulaa@gmail.com>
2025-06-01 16:38:20 +0545

    feat: update completion for blink

--- home/dot_config/nvim/lua/plugins/lsp/completion.lua
+++ home/dot_config/nvim/lua/plugins/lsp/completion.lua
@@ -1,21 +1,22 @@
 -- emacs bahavior: https://cmp.saghen.dev/recipes.html#emacs-behavior
+
+-- EMacs style tab complection and cycle, best with ghost text
 -- local has_words_before = function()
 -- 	local col = vim.api.nvim_win_get_cursor(0)[2]
 -- 	if col == 0 then
 -- 		return false
 -- 	end
---
--- 	local line = vim.api.nvim_get_current_line():sub()
+-- 	local line = vim.api.nvim_get_current_line()
 -- 	return line:sub(col, col):match("%s") == nil
 -- end
 
--- EMacs style tab complection and cycle, best with ghost text
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	if col == 0 then
 		return false
 	end
 	local text = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+
 	return text:sub(col, col):match("%s") == nil
 end
 
@@ -24,25 +25,25 @@ return {
 	event = "InsertEnter",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
-		-- "onsails/lspkind.nvim",
 	},
 	version = "*",
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-
 		appearance = {
 			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 
 		completion = {
-			accept = { auto_brackets = { enabled = true } },
+			accept = {
+				auto_brackets = { enabled = true },
+			},
 
 			documentation = {
 				auto_show = true,
-				auto_show_delay_ms = 250,
+				auto_show_delay_ms = 200, -- Slightly faster
 				treesitter_highlighting = true,
 				window = { border = "rounded" },
 			},
@@ -50,26 +51,24 @@ return {
 			list = {
 				selection = {
 					preselect = true,
-					auto_insert = true,
-
-					-- or a function
-					-- preselect = function(ctx)
-					--   return not require("blink.cmp").snippet_active { direction = 1 }
-					-- end,
-					-- auto_insert = function(ctx)
-					--   return vim.bo.filetype ~= "markdown"
-					-- end,
+					auto_insert = false, -- Better for Emacs-style workflow
 				},
 			},
 
-			ghost_text = { show_with_menu = true, enabled = true },
+			ghost_text = {
+				enabled = true,
+				show_with_menu = true,
+			},
+
 			menu = {
-				auto_show = true,
-				-- border = "rounded",
+				auto_show = function(ctx)
+					-- Show menu automatically but only when it makes sense
+					return ctx.mode ~= "cmdline" and has_words_before()
+				end,
 
 				cmdline_position = function()
 					if vim.g.ui_cmdline_pos ~= nil then
-						local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+						local pos = vim.g.ui_cmdline_pos
 						return { pos[1] - 1, pos[2] }
 					end
 					local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
@@ -77,12 +76,6 @@ return {
 				end,
 
 				draw = {
-					-- columns = {
-					--   { "kind_icon", "label", gap = 2 },
-					--   { "kind" },
-					-- },
-
-					-- columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind", "source_name", gap = 2 } },
 					columns = {
 						{ "kind_icon", "label", "label_description", gap = 1 },
 						{ "kind", "source_name", gap = 1 },
@@ -141,75 +134,117 @@ return {
 			},
 		},
 
-		-- My super-TAB configuration
+		-- Optimized keymap combining Emacs-style with modern UX
 		keymap = {
+			-- Show/hide completion
 			["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
-			-- ["<CR>"] = { "accept", "fallback" },
-			["<TAB>"] = { "accept", "fallback" },
 
-			-- -- ["<Tab>"] = {
-			-- --   function(cmp)
-			-- --     return cmp.select_next()
-			-- --   end,
-			-- --   "snippet_forward",
-			-- --   "fallback",
-			-- -- },
-			-- -- ["<S-Tab>"] = {
-			-- --   function(cmp)
-			-- --     return cmp.select_prev()
-			-- --   end,
-			-- --   "snippet_backward",
-			-- --   "fallback",
-			-- -- },
-			--
-			-- emacs style completion
-			-- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
-			["<TAB>"] = {
+			-- Accept completion
+			["<CR>"] = { "accept", "fallback" },
+
+			-- Emacs-style TAB behavior with smart fallback
+			["<Tab>"] = {
 				function(cmp)
+					-- if cmp.is_visible() then
+					-- 	return cmp.accept()
+					-- end
 					if has_words_before() then
 						return cmp.insert_next()
 					end
+					-- return false
 				end,
 				"fallback",
 			},
-			-- Navigate to the previous suggestion or cancel completion if currently on the first one.
-			["<S-Tab>"] = { "insert_prev" },
+
+			["<S-Tab>"] = {
+				function(cmp)
+					-- if cmp.is_visible() then
+					-- 	return cmp.select_prev()
+					-- end
+					if has_words_before() then
+						return cmp.insert_prev()
+					end
+					-- return false
+				end,
+				"fallback",
+			},
 
 			["<Up>"] = { "select_prev", "fallback" },
 			["<Down>"] = { "select_next", "fallback" },
+
 			["<C-p>"] = { "select_prev", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
+
+			-- Documentation scrolling
 			["<C-u>"] = { "scroll_documentation_up", "fallback" },
 			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+
+			-- Alternative accept keys for flexibility
+			["<C-y>"] = { "accept", "fallback" }, -- Emacs-style accept
+			["<C-Space>"] = { "show", "fallback" }, -- Manual trigger
 		},
 
-		-- Experimental signature help support
+		-- Signature help
 		signature = {
 			enabled = true,
 			window = { border = "rounded" },
 		},
 
+		-- Optimized sources configuration
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
 				lsp = {
 					min_keyword_length = 2, -- Number of characters to trigger porvider
 					score_offset = 0, -- Boost/penalize the score of the items
+					-- Only show LSP items when they're likely relevant
+					enabled = function()
+						return vim.bo.buftype ~= "prompt"
+					end,
 				},
 				path = {
 					min_keyword_length = 0,
+					-- Show path completions in strings and comments
+					enabled = function()
+						local line = vim.api.nvim_get_current_line()
+						local col = vim.api.nvim_win_get_cursor(0)[2]
+						local before_cursor = line:sub(1, col)
+						return before_cursor:match("['\"`]") ~= nil or before_cursor:match("%.%./") ~= nil
+					end,
 				},
 				snippets = {
 					min_keyword_length = 2,
+					score_offset = -1, -- Slightly lower priority than LSP
 				},
 				buffer = {
-					min_keyword_length = 5,
-					max_items = 5,
+					min_keyword_length = 3, -- Balanced
+					max_items = 8, -- More options
+					-- Don't show buffer completions in very small or large buffers
+					enabled = function()
+						local line_count = vim.api.nvim_buf_line_count(0)
+						return line_count > 5 or line_count < 1000
+					end,
 				},
+			},
+
+			-- Per-filetype source configuration
+			per_filetype = {
+				lua = { "lsp", "path", "snippets", "buffer" },
+				-- Disable buffer completions in large files for performance
+				-- javascript = function()
+				-- 	if vim.api.nvim_buf_line_count(0) > 1000 then
+				-- 		return { "lsp", "path", "snippets" }
+				-- 	end
+				-- 	return { "lsp", "path", "snippets", "buffer" }
+				-- end,
 			},
 		},
 
-		fuzzy = { implementation = "prefer_rust_with_warning" },
+		fuzzy = {
+			implementation = "prefer_rust_with_warning",
+			use_frecency = true,
+			use_proximity = true,
+		},
 	},
 }
--- home/dot_config/nvim/lua/plugins/lsp/mason.lua
+++ home/dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -1,89 +1,88 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    event = "VeryLazy",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "dockerfile-language-server",
-        "yaml-language-server",
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"dockerfile-language-server",
+				"yaml-language-server",
 
-        -- golang
-        "gopls",
-        "golangci-lint",
-        "goimports",
-        "golines",
-        "gomodifytags",
-        "impl",
-        -- formatting
-        "biome",
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "ruff",
-        "yamlfmt",
-        "sql-formatter",
+				-- golang
+				"golangci-lint",
+				"goimports",
+				"golines",
+				"gomodifytags",
+				"impl",
+				-- formatting
+				"biome",
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"ruff",
+				"yamlfmt",
+				"sql-formatter",
 
-        "pylint", -- python linter
-        "eslint_d", -- js linter
-        "luacheck", -- linter for lua
-        -- "gofmt",
-        -- "rustfmt",
-        "codelldb",
-        "cpptools",
-      },
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+				"luacheck", -- linter for lua
+				-- "gofmt",
+				-- "rustfmt",
+				"codelldb",
+				"cpptools",
+			},
 
-      ui = {
-        icons = {
-          package_pending = " ",
-          package_installed = "󰄳 ",
-          package_uninstalled = "󰇚 ",
-        },
+			ui = {
+				icons = {
+					package_pending = " ",
+					package_installed = "󰄳 ",
+					package_uninstalled = "󰇚 ",
+				},
 
-        keymaps = {
-          toggle_server_expand = "<CR>",
-          install_server = "i",
-          update_server = "u",
-          check_server_version = "c",
-          update_all_servers = "U",
-          check_outdated_servers = "C",
-          uninstall_server = "X",
-          cancel_installation = "<C-c>",
-        },
-      },
-    },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    config = function()
-      local mason_lspconfig = require "mason-lspconfig"
+				keymaps = {
+					toggle_server_expand = "<CR>",
+					install_server = "i",
+					update_server = "u",
+					check_server_version = "c",
+					update_all_servers = "U",
+					check_outdated_servers = "C",
+					uninstall_server = "X",
+					cancel_installation = "<C-c>",
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		config = function()
+			local mason_lspconfig = require("mason-lspconfig")
 
-      mason_lspconfig.setup {
-        -- list of servers for mason to install
-        ensure_installed = {
-          "ts_ls",
-          "html",
-          "htmx",
-          "cssls",
-          "tailwindcss",
-          "svelte",
-          "lua_ls",
-          "emmet_ls",
-          "prismals",
-          "pyright",
-          "rust_analyzer",
+			mason_lspconfig.setup({
+				-- list of servers for mason to install
+				ensure_installed = {
+					"ts_ls",
+					"html",
+					"htmx",
+					"cssls",
+					"tailwindcss",
+					"svelte",
+					"lua_ls",
+					"emmet_ls",
+					"prismals",
+					"pyright",
+					"rust_analyzer",
 
-          "gopls",
-          "golangci_lint_ls",
-          "templ",
+					-- "gopls",
+					"golangci_lint_ls",
+					"templ",
 
-          -- "sqls", -- for sql
+					-- "sqls", -- for sql
 
-          -- "graphql",
-        },
-        -- auto-install configured servers (with lspconfig)
-        automatic_installation = false, -- not the same as ensure_installed
-      }
-    end,
-  },
+					-- "graphql",
+				},
+				-- auto-install configured servers (with lspconfig)
+				automatic_installation = false, -- not the same as ensure_installed
+			})
+		end,
+	},
 }
